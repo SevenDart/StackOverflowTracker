8e0606243a88279cb69fd99d19340c0a400ecff9
Pavel Slyaz <sir.sevendart@gmail.com>
2021-11-27 01:03:19 +0300

    Added today visits functionality, refactored retrieving information

--- background.js
+++ background.js
@@ -1,19 +1,35 @@
 //main listener
 chrome.webNavigation.onCommitted.addListener((details) => {
     if (details.url.startsWith('https://stackoverflow.com/')) {
-        console.log('catched!', details.timeStamp);
         updateTotalVisits();
+        updateTodayVisits();
     }
 })
 
 function updateTotalVisits() {
     chrome.storage.sync.get(['totalVisits'], (result) => {
         let totalVisits = result.totalVisits;
-        if (!totalVisits) {
-            chrome.storage.sync.set({totalVisits: 0});
-            totalVisits = 0;
-        }
         totalVisits += 1;
         chrome.storage.sync.set({totalVisits: totalVisits});
     });
+}
+
+function updateTodayVisits() {
+    chrome.storage.sync.get(['todayVisits', 'lastVisit'], (result) => {
+        let currentDate = new Date();
+        let todayVisits = result.todayVisits;
+        let lastVisit = new Date(result.lastVisit);
+        if (lastVisit.getFullYear() !== currentDate.getFullYear()
+            || lastVisit.getMonth() !== currentDate.getMonth()
+            || lastVisit.getDate() !== currentDate.getDate()) {
+            todayVisits = 0;
+            chrome.storage.sync.set({lastVisit: Date.now()});
+        }
+        todayVisits += 1;
+        chrome.storage.sync.set({todayVisits: todayVisits, lastVisit: Date.now()});
+    });
+}
+
+{
+    chrome.storage.sync.set({totalVisits: 0, todayVisits: 0, lastVisit: Date.now()});
 }
--- popup-menu/popup-menu.js
+++ popup-menu/popup-menu.js
@@ -1,27 +1,22 @@
 function retrieveInformation() {
-    chrome.storage.sync.get(['todayVisits'], (result) => {
+    chrome.storage.sync.get(['todayVisits', 'totalVisits', 'lastVisit'], (result) => {
         let todayVisits = result.todayVisits;
-        if (!todayVisits) {
-            chrome.storage.sync.set({todayVisits: 0});
+        let totalVisits = result.totalVisits;
+        let currentDate = new Date();
+        let lastVisit = new Date(result.lastVisit);
+        if (lastVisit.getFullYear() !== currentDate.getFullYear()
+            || lastVisit.getMonth() !== currentDate.getMonth()
+            || lastVisit.getDate() !== currentDate.getDate()) {
             todayVisits = 0;
+            chrome.storage.sync.set({todayVisits: 0});
         }
-
-        chrome.storage.sync.get(['totalVisits'], (result) => {
-            let totalVisits = result.totalVisits;
-            if (!totalVisits) {
-                chrome.storage.sync.set({totalVisits: 0});
-                totalVisits = 0;
-            }
-            updatePopup(todayVisits, totalVisits);
-        });
+        updatePopup(todayVisits, totalVisits);
     });
 }
 
 function updatePopup(todayVisits, totalVisits) {
     let todayCountElement = document.getElementById('today-count');
     let totalCountElement = document.getElementById('total-count');
-
-    console.log(totalVisits);
 
     todayCountElement.textContent = todayVisits;
     totalCountElement.textContent = totalVisits;
